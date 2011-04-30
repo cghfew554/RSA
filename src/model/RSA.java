e81800488ca66b75fe9ec56f994bddba1b84301f
Marcel Boersma <boersma.marcel@gmail.com>
2011-04-30 15:48:34 +0200

    isPrime is added, and a list with all the primes up to 1000 and there n values;

--- src/model/RSA.java
+++ src/model/RSA.java
@@ -1,16 +1,77 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Observable;
 
 public class RSA extends Observable{
 	private int primeA = 0;
 	private int primeB = 0;
 	
+	Collection<Integer> primes;
+	HashMap<Integer, RSAKeys> keys;
 	
 	private int privateKey = 0;
 	private int publicKey = 0;
 	
 	public RSA(){
+		
+		this.primes = new ArrayList<Integer>();
+		this.keys   = new HashMap<Integer, RSAKeys>();
+		
+		 for ( int i = 2; i <= 1000; i++ )
+		   {
+		     if ( isPrime ( i ) )
+		    	 this.primes.add(i);
+		     
+		   }
+		 
+		 Iterator<Integer> i = this.primes.iterator();
+		 
+		 //TODO optimize algorithm
+		 while(i.hasNext())
+		 {
+			 int primeAa = i.next();
+			 
+			 Iterator<Integer> b = this.primes.iterator();
+			 
+			 while(b.hasNext())
+			 {
+				 int primeBb = b.next();
+				 System.out.println("PrimeBb : "+primeBb + " ");
+				 this.keys.put((primeAa*primeBb), new RSAKeys(primeAa, primeBb));
+				 System.out.println("Public key :" + (primeAa*primeBb) + " private key pair :"+ primeAa +" : "+primeBb);
+			 }
+		 }
+	
+		
+		 
+	}
+	
+	/**
+	 * Checks if a number is a prime number
+	 * 
+	 * @require prime =< 1000
+	 * @param prime
+	 * @return true if it is a prime and false if not
+	 */
+	public boolean isPrime(int prime)
+	{
+		boolean isPrime = true;
+		
+		
+		for(int i = 2; i < prime; i++)
+		{
+			if(prime % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		
+		return isPrime;
 		
 	}
 	
@@ -21,11 +82,13 @@ public class RSA extends Observable{
 	 * @param prime a prime numbers 
 	 */
 	public void setPrimeA(int prime){
-		this.primeA = prime;
+		if(prime <= 1000 && this.isPrime(prime))
+			this.primeA = prime;
 		
 		this.notifyObserversSync();
 		
 	}
+	
 	
 	/**
 	 * Get prime a
@@ -45,7 +108,8 @@ public class RSA extends Observable{
 	 * @param prime a prime numbers 
 	 */
 	public void setPrimeB(int prime){
-		this.primeB = prime;
+		if(prime <= 1000 && this.isPrime(prime))
+			this.primeB = prime;
 		
 		this.notifyObserversSync();
 	}
